c8253cb5cd293a79e7200dc5557c8cbbb6de6e21
blueboxers <hshavok@gmail.com>
2017-03-30 11:08:35 +0400

    first commit

---
+++ src/pong/pong.java
@@ -0,0 +1,27 @@
+package pong;
+
+import javax.swing.JFrame;
+
+
+
+public class pong extends JFrame
+{
+    JFrame pong = new JFrame();
+            
+    
+    public pong() 
+    {
+       pong.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       pong.setSize(WIDTH,HEIGHT);
+       pong.setResizable(false);
+
+       
+                
+        
+    }
+            
+           
+    
+    
+}
+        
