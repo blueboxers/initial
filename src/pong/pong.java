6766a242cd7767a452781b56fd110a757f8ff55e
blueboxers <hshavok@gmail.com>
2017-03-30 13:35:09 +0400

    0.05

--- src/pong/pong.java
+++ src/pong/pong.java
@@ -4,29 +4,37 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 
-
-public class pong
+public class pong extends JFrame
 {
+    
+    JFrame pong = new JFrame();
+    
+    public static int WIDTH = 800, HEIGHT = 800;
+   
+  
+    public pong ()
+    {
+      pong.setTitle("Sooper Pung");
+      pong.setSize(WIDTH, HEIGHT);
+     
+      pong.setVisible(true);
+      pong.setResizable(false);
+      pong.setLocationRelativeTo(null);
+      pong.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      pong.setFocusable(true);
+      pong.requestFocus();
+      
+      
+      
+      pack();
+    }
+        
+    
     
     
  public static void main(String[] args)
     { 
-        //This is where the application actually starts running
-        
-        //and here we create our pong object, which is a jframe. we give it the
-        //width and height arguments.
-        
-        int WIDTH = 800;
-        int HEIGHT = 800;
-        JFrame pongFrame = new JFrame();
-        
-       pongFrame.setVisible(true);
-       pongFrame.setResizable(false);
-       pongFrame.setLocationRelativeTo(null);
-       pongFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-       pongFrame.setSize(WIDTH, HEIGHT);
-       pongFrame.setBackground(Color.BLACK);
+        new pong();
     }
             
               
